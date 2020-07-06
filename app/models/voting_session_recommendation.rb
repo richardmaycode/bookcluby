0ab9aa33bc1f8ce32016de047e6aa49164a08137
Richard Wise <orsusbass@gmail.com>
2020-07-06 15:14:39 -0500

    added approve/reject recommendations

--- app/models/voting_session_recommendation.rb
+++ app/models/voting_session_recommendation.rb
@@ -2,4 +2,13 @@ class VotingSessionRecommendation < ApplicationRecord
   belongs_to :voting_session
   belongs_to :recommendation
   has_many :votes
+
+  def approve_recommendation
+    update_column(:outcome, "approved")
+    recommendation.update(approved: true, eligible: false)
+  end
+
+  def reject_recommendation
+    update_column(:outcome, "rejected")
+  end
 end
--- spec/models/voting_session_recommendation_spec.rb
+++ spec/models/voting_session_recommendation_spec.rb
@@ -1,9 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe VotingSessionRecommendation, type: :model do
+  subject(:voting_session_recommendation) { create(:voting_session_recommendation) } 
+
   describe "associations" do
     it { should belong_to(:voting_session) } 
     it { should belong_to(:recommendation) } 
     it { should have_many(:votes) }
+  end
+
+  describe "#approve_recommendation" do
+    it 'sets outcome to approved' do
+      voting_session_recommendation.approve_recommendation
+      expect(voting_session_recommendation.outcome).to eq "approved"  
+    end
+    it 'sets its recommendation to approved' do
+      voting_session_recommendation.approve_recommendation
+      expect(voting_session_recommendation.recommendation.approved).to eq true
+    end
+    it 'sets its reccomendation to ineligible for future voting' do
+      voting_session_recommendation.approve_recommendation
+      expect(voting_session_recommendation.recommendation.eligible).to eq false
+    end
+  end
+
+  describe "#reject_recommendation" do
+    it "sets outcome to declined" do
+      voting_session_recommendation.reject_recommendation  
+      expect(voting_session_recommendation.outcome).to eq "rejected" 
+    end
   end
 end
