de999df3cbe09c4f79c75c1083caf2284f7b6f3d
Richard Wise <orsusbass@gmail.com>
2020-07-06 16:03:54 -0500

    create routes for reads and recommendations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :groups
+  resources :groups do
+    resources :recommendations, only: %w[index]
+    resources :reads, only: %w[index show]
+  end
 end
