915b9f09718ad0c0598f69c2b4f5279a32b25804
Richard Wise <orsusbass@gmail.com>
2020-07-06 15:58:19 -0500

    create group control/routes/views

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,55 @@
+class GroupsController < ApplicationController
+  before_action :set_group, only: w%[show edit update destroy]
+
+  def index
+    @groups = Group.all
+  end
+  
+  def show; end
+
+  def new
+    @group = Group.new
+  end
+  
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:success] = "Group successfully created"
+      redirect_to @group
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  def edit; end
+  
+  def update
+    if @group.update(group_params)
+      flash[:success] = "Group was successfully updated"
+      redirect_to @group
+    else
+      flash[:error] = "Something went wrong"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    if @group.destroy
+      flash[:success] = 'Group was successfully deleted.'
+      redirect_to groups_url
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to groups_url
+    end
+  end
+  
+  private
+  def set_group
+    @group = Group.find(parmas[:id])
+  end
+
+  def group_params
+    params.require(:group).permit()
+  end
+end
